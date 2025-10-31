6c9ed3a58758c3494fd26e31872c90aeeb3127ff
jerrychir <jerrychir@gmail.com>
2025-10-31 21:41:20 +0800

    add rerank and tei

--- rig-bailian/src/lib.rs
+++ rig-bailian/src/lib.rs
@@ -8,7 +8,7 @@ pub mod types;
 pub use client::Client;
 pub use completion::CompletionModel;
 pub use embedding::{EmbeddingModel, TEXT_EMBEDDING_V4};
-pub use rerank::{GTE_RERANK_V2, RerankError, RerankModel, RerankResult};
+pub use rerank::{RerankError, RerankModel, RerankResult, GTE_RERANK_V2};
 
 use rig::impl_conversion_traits;
 
